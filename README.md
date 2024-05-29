2479afccad782fcef4f2c7c1a15f2acb499bd3b1
Shankhadeep Sarkar <75299574+Shankhadeep2000@users.noreply.github.com>
2024-05-29 19:56:33 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,45 @@
-# TextMuse
+# Creative Writing Generator Webapp
+Creative writing sentence generator using user input prompts. AI images are generated from the prompt results.
+
+Coded using the OpenAI API, HTML/CSS and Python (Flask) backend.
+Powered by GPT-3.5. 
+
+AI generated images from stable diffusion.
+
+Preview Demo (v1):
+
+With AI-generated images (stable diffusion): 
+
+![gif](https://github.com/lulu-wang/Creative-Writing-AI-Webapp/assets/16969709/3e8b3127-fc80-470a-8f57-69e57d10f656)
+
+Longer:
+![gif2](https://github.com/lulu-wang/Creative-Writing-AI-Webapp/assets/16969709/56d20bef-d580-4916-87c1-f42eca069006)
+
+
+
+## Setup (local)
+
+1. Navigate into the project directory.
+
+2. Create a new virtual environment:
+
+   ```bash
+   $ python -m venv venv
+   $ . venv/bin/activate
+   ```
+
+3. Install the requirements:
+
+   ```bash
+   $ pip install -r requirements.txt
+   ```
+   
+   Make sure to have API keys for Open AI and Stable Diffusion.
+
+4. Run the app:
+
+   ```bash
+   $ flask run
+   ```
+
+You can see the app at [http://localhost:5000](http://localhost:5000)
---
+++ app.py
@@ -0,0 +1,88 @@
+import os
+import openai
+import requests
+import json
+from flask import Flask, redirect, render_template, request, url_for
+
+app = Flask(__name__)
+openai.api_key = os.getenv("OPENAI_API_KEY")
+stableDiffusionKey = os.getenv("STABLEDIFFUSION_API_KEY")
+stable_diffusion_url = "https://stablediffusionapi.com/api/v3/text2img"
+
+
+@app.route("/", methods=("GET", "POST"))
+def index():
+    pictureUrl = ""
+    if request.method == "POST":
+        userText = request.form["userText"]
+        # response = openai.Completion.create(
+        #     model="gpt-3.5-turbo",
+        #     prompt=generate_chat_prompt(userText),
+        #     temperature=0.6,
+        # )
+        response = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo",
+            temperature=0.7,
+            messages=[
+                {"role": "system", "content": "You are generating literary sentences based on the prompt word or words."},
+                {"role": "user", "content": "England"},
+                {"role": "assistant", "content": "As the sun set, England was lit up in a glowing light."},
+                {"role": "user", "content": "Butterfly"},
+                {"role": "assistant", "content": "A butterfly landed on my finger in the garden."},
+                {"role": "user", "content": userText}
+            ],
+        )
+
+        resultText = response['choices'][0]['message']['content']
+        print (stableDiffusionKey)
+
+        payload = json.dumps({
+            "key": stableDiffusionKey,
+            "prompt": resultText,
+            "negative_prompt": "nudity",
+            "width": "512",
+            "height": "512",
+            "samples": "1",
+            "num_inference_steps": "20",
+            "seed": None,
+            "guidance_scale": 7.5,
+            "safety_checker": "yes",
+            "multi_lingual": "no",
+            "panorama": "no",
+            "self_attention": "no",
+            "upscale": "no",
+            "embeddings_model": stable_diffusion_url,
+            "webhook": None,
+            "track_id": None
+        })
+
+        headers = {
+            'Content-Type': 'application/json'
+        }
+
+        responsePicture = requests.request("POST", stable_diffusion_url, headers=headers, data=payload)
+        responseJson = responsePicture.json()
+
+        if "output" in responseJson:
+            pictureUrl = responseJson["output"]
+        print(responseJson)
+
+        return redirect(url_for("index", result=resultText, pictureUrl=pictureUrl))
+
+    result = request.args.get("result")
+    pictureUrl = request.args.get("pictureUrl")
+    return render_template("index.html", result=result, pictureUrl=pictureUrl)
+
+def generate_chat_prompt(topic):
+    return """Write a proper sentence based on the prompt word.
+    Prompt: England
+    Sentence: As the sun set, England was lit up in a glowing light.
+    Prompt: Butterfly
+    Sentence: A butterfly landed on my finger in the garden.
+    Prompt: {}
+    Sentence:""".format(
+        topic.capitalize()
+    )
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=5000)
